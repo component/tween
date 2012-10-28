0e77a891c9d15f09ffc9cebcf8ede04e0c4c4905
TJ Holowaychuk <tj@vision-media.ca>
2012-10-28 02:34:37 -0700

    docs

--- index.js
+++ index.js
@@ -6,7 +6,18 @@
 var Emitter = require('emitter')
   , ease = require('ease');
 
+/**
+ * Expose `Tween`.
+ */
+
 module.exports = Tween;
+
+/**
+ * Initialize a new `Tween` with `obj`.
+ *
+ * @param {Object} obj
+ * @api public
+ */
 
 function Tween(obj) {
   if (!(this instanceof Tween)) return new Tween(obj);
@@ -16,7 +27,17 @@ function Tween(obj) {
   this.duration(500);
 }
 
+/**
+ * Mixin emitter.
+ */
+
 Emitter(Tween.prototype);
+
+/**
+ * Reset the tween.
+ *
+ * @api public
+ */
 
 Tween.prototype.reset = function(){
   this._curr = clone(this._from);
@@ -25,15 +46,43 @@ Tween.prototype.reset = function(){
   return this;
 };
 
+/**
+ * Tween to `obj`.
+ *
+ *    tween.to({ x: 50, y: 100 })
+ *
+ * @param {Object} obj
+ * @return {Tween} self
+ * @api public
+ */
+
 Tween.prototype.to = function(obj){
   this._to = obj;
   return this;
 };
 
+/**
+ * Set duration to `ms` [500].
+ *
+ * @param {Number} ms
+ * @return {Tween} self
+ * @api public
+ */
+
 Tween.prototype.duration = function(ms){
   this._duration = ms;
   return this;
 };
+
+/**
+ * Set easing function to `fn`.
+ *
+ *    tween.ease('in-out-sine')
+ *
+ * @param {String|Function} fn
+ * @return {Tween}
+ * @api public
+ */
 
 Tween.prototype.ease = function(fn){
   fn = 'function' == typeof fn ? fn : ease[fn];
@@ -41,6 +90,13 @@ Tween.prototype.ease = function(fn){
   this._ease = fn;
   return this;
 };
+
+/**
+ * Perform a step.
+ *
+ * @return {Tween} self
+ * @api private
+ */
 
 Tween.prototype.step = function(){
   if (this._done) return;
@@ -70,7 +126,18 @@ Tween.prototype.step = function(){
   }
 
   this._update(curr);
+  return this;
 };
+
+/**
+ * Set update function to `fn` or 
+ * when no argument is given this performs
+ * a "step".
+ *
+ * @param {Function} fn
+ * @return {Tween} self
+ * @api public
+ */
 
 Tween.prototype.update = function(fn){
   if (0 == arguments.length) return this.step();
