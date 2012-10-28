c178797602d39401807e2ef32d9d6c1424c461fa
TJ Holowaychuk <tj@vision-media.ca>
2012-10-28 02:17:38 -0700

    bulk of the implementation

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ module.exports = Tween;
 
 function Tween(obj) {
   if (!(this instanceof Tween)) return new Tween(val);
-  this._start = obj;
+  this._from = obj;
+  this._curr = clone(obj);
   this.ease('linear');
   this.duration(500);
 }
@@ -30,15 +31,50 @@ Tween.prototype.duration = function(ms){
 Tween.prototype.ease = function(fn){
   fn = 'function' == typeof fn ? fn : ease[fn];
   if (!fn) throw new TypeError('invalid easing function');
-  this.ease = fn;
+  this._ease = fn;
+};
+
+Tween.prototype.step = function(){
+  // duration
+  var duration = this._duration;
+  var now = Date.now();
+  var diff = now - this._start;
+  var done = diff >= duration;
+  if (done) return this.emit('end');
+
+  // tween
+  var obj = this._from;
+  var to = this._to;
+  var curr = this._curr;
+  var fn = this._ease;
+
+  for (var key in obj) {
+    var val = obj[key];
+    curr[key] = fn(diff, val, to[key] - curr[key], duration);
+  }
+
+  this._update(curr);
 };
 
 Tween.prototype.update = function(fn){
+  if (0 == arguments.length) return this.step();
   this._update = fn;
   return this;
 };
 
 
 Tween.prototype.start = function(){
-  
+  this._start = Date.now();
 };
+
+/**
+ * Clone `obj`.
+ * 
+ * @api private
+ */
+
+function clone(obj) {
+  var ret = {};
+  for (var key in obj) ret[key] = obj[key];
+  return ret;
+}
